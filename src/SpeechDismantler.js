827389dfa4772371ec68a84014ff2870f6eb8cd5
Aleksi <aleksi.suoranta@gmail.com>
2019-03-13 18:05:15 +0200

    Transcription and iterim now displayed, Added Reset button, Refactored the code, added Eslint

--- src/SpeechDismantler.js
+++ src/SpeechDismantler.js
@@ -1,287 +1,224 @@
-import React, { Component } from 'react';
-import { Button, Dropdown, Navbar } from 'react-bootstrap';
-import { VictoryBar, VictoryTheme, VictoryChart, VictoryPie } from 'victory';
-import openSocket from 'socket.io-client';
+import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
+// import { VictoryBar, VictoryTheme, VictoryChart, VictoryPie } from 'victory';
+import openSocket from 'socket.io-client'
+import PropTypes from 'prop-types'
 
-let bufferSize = 2048,
-  AudioContext,
-  context,
-  processor,
-  input,
-  globalStream,
-  streamStreaming = false;
-//const socket = new WebSocket("ws://localhost:8000");
-const socket = openSocket('http://localhost:3001');
+let AudioContext
+let context
+let processor
+let input
+let globalStream
 
+const Transcript = ({ transcript }) => {
+	const final = {
+		color: 'black',
+		border: '#ccc 1px solid',
+		padding: '1em',
+		margin: '1em',
+		width: '500px',
+	}
+	return (
+		<div id="final" style={final}>{transcript}</div>
+	)
+}
 
+Transcript.propTypes = {
+	transcript: PropTypes.string
+}
 
+const Interim = ({ interim }) => {
+	const interimStyle = {
+		color: 'gray',
+		border: '#ccc 1px solid',
+		padding: '1em',
+		margin: '1em',
+		width: '500px',
+	}
+	return (
+		<div id="interim" style={interimStyle}>{interim}</div>
+	)
+}
 
+Interim.propTypes = {
+	interim: PropTypes.string
+}
 
-
-
-
+const initialState = {
+	isRecording: false,
+	transcript: '',
+	interim: '',
+}
 
 class SpeechDismantler extends Component {
+	constructor(props) {
+		super(props)
+		this.bufferSize = 2048
+		this.socket = openSocket('http://localhost:3001')
+		this.state = initialState
 
-  constructor(props) {
-    super(props);
-    this.bufferSize = 4096;
-    this.socket = openSocket('http://localhost:3001');
-    this.state = {
-      isRecording: false,
-      isAnalyzing: false
-    }
+		this.socket.on('connect', () => {
+			this.socket.emit('join', 'Server Connected to Client')
+		})
 
-    this.socket.on('connect', (data) => {
-      this.socket.emit('join', 'Server Connected to Client');
-    });
-    
-    this.socket.on('messages', (data) => {
-      console.log(data);
-    });
+		this.socket.on('messages', () => {
+		})
 
-    this.socket.on('speechData', (data) => {
-      console.log(data.results[0].alternatives[0].transcript);
-    })
+		this.socket.on('speechData', (data) => {
+			const final = undefined || data.results[0].isFinal
+			const result = data.results[0].alternatives[0].transcript
+			if (final === false) {
+				// The returned transcript is not finished
+				this.setState({
+					interim: result,
+				})
+			} else {
+				const newTranscript = `${this.state.transcript} ${result}`
+				this.setState({
+					transcript: newTranscript,
+				})
+			}
+		})
 
-    window.onbeforeunload = () =>{
-      if (streamStreaming) { this.socket.emit('endGoogleCloudStream', ''); }
-    };
-    
-  }
+		window.onbeforeunload = () => {
+			if (this.state.isRecording) { this.socket.emit('endGoogleCloudStream', '') }
+		}
+	}
 
-
-  toggleRecord = () => {
-    console.log("p")
-    this.setState({
-      isRecording: !this.state.isRecording
-    }, this.handleListen);
-  }
-
-  startAnalyze = () => {
-    this.setState(state => ({
-      isAnalyzing: !state.isAnalyzing
-    }));
-  }
-
-  streamAudioData = (e) => {
-    //debugger
-    var left = e.inputBuffer.getChannelData(0);
-    // var left16 = convertFloat32ToInt16(left); // old 32 to 16 function
-    var left16 = this.downsampleBuffer(left, 44100, 16000)
-    this.socket.emit('binaryData', left16);
-  }
-
-  handleListen = () => {
-    this.socket.emit('startGoogleCloudStream', ''); //init socket Google Speech Connection
-    streamStreaming = true;
-    AudioContext = window.AudioContext || window.webkitAudioContext;
-    context = new AudioContext();
-    processor = context.createScriptProcessor(bufferSize, 1, 1);
-    processor.connect(context.destination);
-    context.resume();
-
-    const handleSuccess = (stream) => {
-      console.log(this)
-      globalStream = stream;
-      input = context.createMediaStreamSource(stream);
-      input.connect(processor);
-      processor.onaudioprocess = (e) => {
-        this.streamAudioData(e)
-      };
-      console.log("init done")
-    }
-
-    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-      .then(handleSuccess)
-  }
-
-  stopRecording = () => {
-    // waited for FinalWord
-    /*
-    startButton.disabled = false;
-    endButton.disabled = true;
-    recordingStatus.style.visibility = "hidden";
-    */
-    streamStreaming = false;
-    this.socket.emit('endGoogleCloudStream', '');
+	reset = () => {
+		if (this.state.isRecording) {
+			this.stopRecording()
+		}
+		this.setState(initialState)
+	}
 
 
-    let track = globalStream.getTracks()[0];
-    track.stop();
+	toggleRecord = () => {
+		const newIsRecording = !this.state.isRecording // state might not be updated if it is read after setState
+		this.setState({
+			isRecording: newIsRecording,
+		},
+			newIsRecording
+				? this.handleListen
+				: this.stopRecording)
+	}
 
-    input.disconnect(processor);
-    processor.disconnect(context.destination);
-    context.close().then(function () {
-      input = null;
-      processor = null;
-      context = null;
-      AudioContext = null;
-    })
-  }
+	streamAudioData = (e) => {
+		const left = e.inputBuffer.getChannelData(0)
+		const left16 = this.downsampleBuffer(left, 44100, 16000)
+		this.socket.emit('binaryData', left16)
+	}
 
-  /*
-  convertFloat32ToInt16 = (buffer) =>{
-    let l = buffer.length;
-    let buf = new Int16Array(l / 3);
-  
-    while (l--) {
-      if (l % 3 == 0) {
-        buf[l / 3] = buffer[l] * 0xFFFF;
-      }
-    }
-    return buf.buffer
-  }
-  */
+	handleListen = () => {
+		this.socket.emit('startGoogleCloudStream', '') // init socket Google Speech Connection
+		AudioContext = window.AudioContext || window.webkitAudioContext
+		context = new AudioContext()
+		processor = context.createScriptProcessor(this.bufferSize, 1, 1)
+		processor.connect(context.destination)
+		context.resume()
 
-  downsampleBuffer = (buffer, sampleRate, outSampleRate) => {
-    if (outSampleRate == sampleRate) {
-        return buffer;
-    }
-    if (outSampleRate > sampleRate) {
-        throw "downsampling rate show be smaller than original sample rate";
-    }
-    var sampleRateRatio = sampleRate / outSampleRate;
-    var newLength = Math.round(buffer.length / sampleRateRatio);
-    var result = new Int16Array(newLength);
-    var offsetResult = 0;
-    var offsetBuffer = 0;
-    while (offsetResult < result.length) {
-        var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
-        var accum = 0, count = 0;
-        for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
-            accum += buffer[i];
-            count++;
-        }
+		const handleSuccess = (stream) => {
+			globalStream = stream
+			input = context.createMediaStreamSource(stream)
+			input.connect(processor)
+			processor.onaudioprocess = (e) => {
+				this.streamAudioData(e)
+			}
+		}
 
-        result[offsetResult] = Math.min(1, accum / count)*0x7FFF;
-        offsetResult++;
-        offsetBuffer = nextOffsetBuffer;
-    }
-    return result.buffer;
+		navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+			.then(handleSuccess)
+	}
+
+	stopRecording = () => {
+		this.socket.emit('endGoogleCloudStream', '')
+
+
+		const track = globalStream.getTracks()[0]
+		track.stop()
+
+		input.disconnect(processor)
+		processor.disconnect(context.destination)
+		context.close().then(() => {
+			input = null
+			processor = null
+			context = null
+			AudioContext = null
+		})
+	}
+
+	downsampleBuffer = (buffer, sampleRate, outSampleRate) => {
+		if (outSampleRate === sampleRate) {
+			return buffer
+		}
+		if (outSampleRate > sampleRate) {
+			throw 'downsampling rate show be smaller than original sample rate'
+		}
+		const sampleRateRatio = sampleRate / outSampleRate
+		const newLength = Math.round(buffer.length / sampleRateRatio)
+		const result = new Int16Array(newLength)
+		let offsetResult = 0
+		let offsetBuffer = 0
+		while (offsetResult < result.length) {
+			const nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio)
+			let accum = 0; let
+				count = 0
+			for (let i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+				accum += buffer[i]
+				count++
+			}
+
+			result[offsetResult] = Math.min(1, accum / count) * 0x7FFF
+			offsetResult++
+			offsetBuffer = nextOffsetBuffer
+		}
+		return result.buffer
+	}
+
+	render() {
+		const parentContainerStyles = {
+			position: 'absolute',
+			height: '100%',
+			width: '100%',
+			display: 'table',
+		}
+
+		const subContainerStyles = {
+			position: 'relative',
+			height: '100%',
+			width: '100%',
+			display: 'table-cell',
+			verticalAlign: 'middle',
+		}
+
+		const container = {
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center',
+			textAlign: 'center',
+		}
+		return (
+			<div style={parentContainerStyles}>
+				<div style={subContainerStyles}>
+					<div className="center-block text-center">
+						<img src="https://image.flaticon.com/icons/svg/149/149046.svg" width="100" className="center-block text-center" />
+					</div>
+					<div className="center-block text-center">
+						<div style={container}>
+							<Button variant={this.state.isRecording ? 'danger' : 'primary'} onClick={this.toggleRecord}>{this.state.isRecording ? 'Stop' : 'Start'}</Button>
+							<Interim interim={this.state.interim} />
+							<Transcript transcript={this.state.transcript} />
+						</div>
+
+						<br />
+						<br />
+						<Button variant="warning" onClick={this.reset}>Reset</Button>
+					</div>
+
+				</div>
+			</div>
+		)
+	}
 }
 
-
-
-
-
-
-
-
-  render (){
-
-
-
-
-    const parentContainerStyles = {
-      position: 'absolute',
-      height: '100%',
-      width: '100%',
-      display: 'table'
-    };
-
-    const subContainerStyles = {
-      position: 'relative',
-      height: '100%',
-      width: '100%',
-      display: 'table-cell',
-      verticalAlign: 'middle'
-    };
-
-    const container = {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      textAlign: 'center'
-    }
-    const button = {
-      width: '60px',
-      height: '60px',
-      background: 'lightblue',
-      borderRadius: '50%',
-      margin: '6em 0 2em 0'
-    }
-    const interim = {
-      color: 'gray',
-      border: '#ccc 1px solid',
-      padding: '1em',
-      margin: '1em',
-      width: '300px'
-    }
-    const final = {
-      color: 'black',
-      border: '#ccc 1px solid',
-      padding: '1em',
-      margin: '1em',
-      width: '300px'
-    }
-
-
-    if (this.state.isAnalyzing) {
-      return (
-        <div>
-          <Navbar bg="dark" sticky="top">
-            <Button onClick={this.startAnalyze}>Back</Button>
-          </Navbar>
-          <h1 align="center">Statistics</h1>
-          <div className="center-block">
-            <VictoryChart
-              theme={VictoryTheme.material}
-              domainPadding={10}
-
-            >
-              <VictoryBar
-                style={{ data: { fill: "#c43a31" } }}
-
-              />
-            </VictoryChart>
-            <VictoryPie
-              data={[
-                { x: "Cats", y: 35 },
-                { x: "Dogs", y: 40 },
-                { x: "Birds", y: 55 }
-              ]}
-            />
-          </div>
-          <div className="center-block text-center">
-
-          </div>
-        </div>);
-    }
-
-    return (
-      <div style={parentContainerStyles}>
-        <div style={subContainerStyles}>
-          <div className="center-block text-center">
-            <h2>WPM: 100</h2>
-            <br></br>
-            <h2>Filler words per 100: 10</h2>
-            <br></br>
-            <h2>Volume: Okay</h2>
-          </div>
-
-          <div className="center-block text-center">
-            <img src={"https://image.flaticon.com/icons/svg/149/149046.svg"} width="100" className="center-block text-center" />
-          </div>
-          <div className="center-block text-center">
-            <div style={container}>
-              <Button variant={this.state.isRecording ? 'danger' : 'primary'} onClick={this.toggleRecord}>{this.state.isRecording ? 'Stop' : 'Start'}</Button>
-              <div id='interim' style={interim}></div>
-              <div id='final' style={final}></div>
-            </div>
-
-            <br></br>
-            <br></br>
-            <Button variant="warning" onClick={this.startAnalyze}>Analyze</Button>
-          </div>
-
-        </div>
-      </div>
-    );
-
-  }
-
-}
-
-export default SpeechDismantler;
-
+export default SpeechDismantler
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SpeechDismantler from './SpeechDismantler';
-import * as V from 'victory';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SpeechDismantler from './SpeechDismantler'
 
 
-ReactDOM.render(<SpeechDismantler/>, document.getElementById('root'));
+ReactDOM.render(<SpeechDismantler />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

ddfb9b3ddc340b91fb4bf439787a2a6135d388af
Aleksi <aleksi.suoranta@gmail.com>
2019-03-24 19:43:35 +0200

    sidebar fixes and code cleaning

--- src/SpeechDismantler.js
+++ src/SpeechDismantler.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import { Button, Paper, Typography, Grid, Fab, Icon, Drawer, SwipeableDrawer, List, ListItem, ListItemText, Hidden } from '@material-ui/core'
+import { Button, Paper, Grid, Fab, Drawer, SwipeableDrawer, List, ListItem, ListItemText, Hidden } from '@material-ui/core'
 import MicIcon from '@material-ui/icons/Mic'
 import PauseIcon from '@material-ui/icons/Pause'
+import MiniDrawer from "./MiniDrawer"
 
 // import { VictoryBar, VictoryTheme, VictoryChart, VictoryPie } from 'victory';
 import openSocket from 'socket.io-client'
 import PropTypes from 'prop-types'
-import { relative } from 'path';
 
 let AudioContext
 let context
@@ -15,13 +15,11 @@ let input
 let globalStream
 
 
-//This is a test comment for Travis builds
-
 const Transcript = ({ transcript }) => {
-	const items = transcript.map((word) => {return <span>{word.word} </span>})
+	const items = transcript.map((word) => { return <span>{word.word} </span> })
 	return (
 		<div>
-			<Paper elevation={3} style={{maxHeight: "30vh", height: "30vh", overflow: "auto"}}>		
+			<Paper elevation={3} style={{ maxHeight: "30vh", height: "30vh", overflow: "auto" }}>
 				{items}
 			</Paper>
 		</div>
@@ -36,7 +34,7 @@ Transcript.propTypes = {
 const Interim = ({ interim }) => {
 	return (
 		<div>
-			<Paper elevation={2} style={{color:"gray", height: "5vh", textAlign: 'center',}}>		
+			<Paper elevation={2} style={{ color: "gray", height: "5vh", textAlign: 'center', }}>
 				{interim}
 			</Paper>
 		</div>
@@ -61,8 +59,9 @@ const initialState = {
 class SpeechDismantler extends Component {
 	constructor(props) {
 		super(props)
+		let { server_address } = props
 		this.bufferSize = 2048
-		this.socket = openSocket('http://localhost:3001')
+		this.socket = openSocket(server_address)
 		//this.socket = openSocket('https://speech-dismantler.herokuapp.com/')
 		this.state = initialState
 
@@ -83,7 +82,7 @@ class SpeechDismantler extends Component {
 				})
 			} else {
 				var newTranscript = this.state.transcript.slice(0)
-				Array.prototype.push.apply(newTranscript,result)
+				Array.prototype.push.apply(newTranscript, result)
 				this.setState({
 					transcript: newTranscript,
 				})
@@ -99,7 +98,7 @@ class SpeechDismantler extends Component {
 		if (this.state.isRecording) {
 			this.stopRecording()
 		}
-		this.setState(initialState, ()=>{console.log(this.state)})
+		this.setState(initialState, () => { console.log(this.state) })
 	}
 
 
@@ -191,158 +190,106 @@ class SpeechDismantler extends Component {
 
 	toggleDrawer = (side, open) => () => {
 		this.setState({
-		  [side]: open,
+			[side]: open,
 		})
 	}
 
-	/*TOGGLE MINI DRAWER FOR BIGGER RESOLUTIONS*/
-
-	handleDrawerOpen = () => {
-		this.setState({ open: true });
-	  }
 	
-	  handleDrawerClose = () => {
-		this.setState({ open: false });
-	}
-
 	/*UI CODE STARTS HERE*/
 
-	
+
 	render() {
 
+		
 		const { classes } = this.props;
+
 
 		const sideListSwipeable = (
 			<div>
-			  <List>
-				{['TEST HEADER 1 SWIPE 1'].map((text, index) => (
-				  <ListItem button key={text}>
-					<ListItemText primary={text} />
-				  </ListItem>
-				))}
-				{['TEST HEADER 1 SWIPE 2'].map((text, index) => (
-				  <ListItem button key={text}>
-					<ListItemText primary={text} />
-				  </ListItem>
-				))}
-			  </List>
+				<List>
+					{['TEST HEADER 1 SWIPE 1'].map((text, index) => (
+						<ListItem button key={text}>
+							<ListItemText primary={text} />
+						</ListItem>
+					))}
+					{['TEST HEADER 1 SWIPE 2'].map((text, index) => (
+						<ListItem button key={text}>
+							<ListItemText primary={text} />
+						</ListItem>
+					))}
+				</List>
 			</div>
 		)
+		
 
 		return (
-		<div>
 
 			<div>
-				<Grid container
-					spacing={24}
-					direction="column"
-					alignItems="left"
-					justify="space-evenly">
-					<Hidden ndUp>
-					<Grid item xs={12} md={12}>
-						<Button onClick={this.toggleDrawer('left', true)}>OPEN SWIPE</Button>
-					</Grid>
-					</Hidden>					
-				</Grid>	
-			</div>			
-			
-
-			<div>
-				<Grid container
-					spacing={24}
-					direction="column"
-					alignItems="center"
-					justify="flex-end"
-					>
-					<Grid item xs={12}>
-						<Fab aria-label="mic" color={this.state.isRecording ? 'secondary' : 'primary'} onClick={this.toggleRecord}>
-							{this.state.isRecording ? <PauseIcon/> : <MicIcon/>}
-						</Fab>
-					</Grid>
-					<Grid item xs={6} md={3} style={{ width: "100%", height: "100%" }}>
-						<Interim interim={this.state.interim} />
-					</Grid>
-					<Grid item xs={12} md={6} style={{ width: "100%", height: "100%" }}>
-						<Transcript transcript={this.state.transcript} />
-					</Grid>
-					<Grid item xs={12}>
-						<Button variant="contained" onClick={this.reset}>Reset</Button>
-					</Grid>
-				</Grid>
-			</div>		
-
-			
-			<div>
-	
-			<Hidden mdUp>
-			<SwipeableDrawer
-          		open={this.state.left}
-          		onClose={this.toggleDrawer('left', false)}
-          		onOpen={this.toggleDrawer('left', true)}
-        	>	
-          		<div
-            		tabIndex={0}
-            		role="button"
-            		onClick={this.toggleDrawer('left', false)}
-            		onKeyDown={this.toggleDrawer('left', false)}
-          		>
-            		{sideListSwipeable}
-          		</div>
-			</SwipeableDrawer>
-			</Hidden>
-
-			</div>
-
-
-			<Hidden smDown>
-			<Drawer
-				open={this.state.open}
-				variant="permanent"
-				/*className={classNames(classes.drawer, {
-					[classes.drawerOpen]: this.state.open,
-					[classes.drawerClose]: !this.state.open,
-				})}
-				classes={{
-					paper: classNames({
-					[classes.drawerOpen]: this.state.open,
-					[classes.drawerClose]: !this.state.open,
-					}),
-				}}*/
-				
-				>
+				{/*
 				<div>
 					<Grid container
 						spacing={24}
 						direction="column"
 						alignItems="left"
 						justify="space-evenly">
-						<Hidden smDown>
+						<Hidden ndUp>
 							<Grid item xs={12} md={12}>
-								<Button onClick={this.handleDrawerOpen}>OPEN MINI</Button>
-							</Grid>
-							<Grid item xs={12} md={12}>
-								<Button onClick={this.handleDrawerCloses}>CLOSE MINI</Button>
+								<Button onClick={this.toggleDrawer('left', true)}>OPEN SWIPE</Button>
 							</Grid>
 						</Hidden>
-						
-					</Grid>	
-				</div>>
-				<List>
-					{['TEST HEADER 2 MINI 1'].map((text, index) => (
-					<ListItem button key={text}>
-						<ListItemText primary={text} />
-					</ListItem>
-					))}
-					{['TEST HEADER 2 MINI 2'].map((text, index) => (
-					<ListItem button key={text}>
-						<ListItemText primary={text} />
-					</ListItem>
-					))}
-				</List>
-				</Drawer>
+					</Grid>
+				</div>
+				*/}
+
+				<Hidden smDown>
+					<MiniDrawer />
 				</Hidden>
 
-		</div>
+				<div>
+					<Hidden mdUp>
+						<SwipeableDrawer
+							open={this.state.left}
+							onClose={this.toggleDrawer('left', false)}
+							onOpen={this.toggleDrawer('left', true)}
+						>
+							<div
+								tabIndex={0}
+								role="button"
+								onClick={this.toggleDrawer('left', false)}
+								onKeyDown={this.toggleDrawer('left', false)}
+							>
+								{sideListSwipeable}
+							</div>
+						</SwipeableDrawer>
+					</Hidden>
+
+				</div>
+
+				<div>
+					<Grid container
+						spacing={24}
+						direction="column"
+						alignItems="center"
+						justify="flex-end"
+					>
+						<Grid item xs={12}>
+							<Fab aria-label="mic" color={this.state.isRecording ? 'secondary' : 'primary'} onClick={this.toggleRecord}>
+								{this.state.isRecording ? <PauseIcon /> : <MicIcon />}
+							</Fab>
+						</Grid>
+						<Grid item xs={6} md={3} style={{ width: "100%", height: "100%" }}>
+							<Interim interim={this.state.interim} />
+						</Grid>
+						<Grid item xs={12} md={6} style={{ width: "100%", height: "100%" }}>
+							<Transcript transcript={this.state.transcript} />
+						</Grid>
+						<Grid item xs={12}>
+							<Button variant="contained" onClick={this.reset}>Reset</Button>
+						</Grid>
+					</Grid>
+				</div>
+
+			</div >
 
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import SpeechDismantler from './SpeechDismantler'
 
-
-ReactDOM.render(<SpeechDismantler />, document.getElementById('root'))
+const server_address = process.env.SERVER_ADDRESS || "http://localhost:3001" 
+ReactDOM.render(<SpeechDismantler server_address={server_address}/>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

15d09640d4c44f00ab660fda99120cf91a5fdf44
Aleksi <aleksi.suoranta@gmail.com>
2019-03-24 22:06:00 +0200

    server address

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SpeechDismantler from './SpeechDismantler'
-
-const server_address = process.env.SERVER_ADDRESS || "http://localhost:3001" 
+var server_address = "https://speech-dismantler.herokuapp.com/" 
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    server_address = "http://localhost:3001" 
 ReactDOM.render(<SpeechDismantler server_address={server_address}/>, document.getElementById('root'))

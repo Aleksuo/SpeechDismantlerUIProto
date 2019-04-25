e9bbfac24589aa8cb279926f805df8bca77f6065
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-25 08:35:00 +0000

    Deploy speech-dismantler-5000/SpeechDismantlerFront to github.com/speech-dismantler-5000/SpeechDismantlerFront.git:gh-pages

---
+++ precache-manifest.2108034ef871cc150e426003c30bf499.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "df24a95f500a0d877a06",
+    "url": "/SpeechDismantlerFront/static/js/main.882ac841.chunk.js"
+  },
+  {
+    "revision": "4202a09ac04294d04a77",
+    "url": "/SpeechDismantlerFront/static/js/runtime~main.da23367c.js"
+  },
+  {
+    "revision": "458356a0f385a42a9fe3",
+    "url": "/SpeechDismantlerFront/static/js/2.ebf69798.chunk.js"
+  },
+  {
+    "revision": "d6ccc49296c352a8b7aa4fedf7637acd",
+    "url": "/SpeechDismantlerFront/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/SpeechDismantlerFront/precache-manifest.2108034ef871cc150e426003c30bf499.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/SpeechDismantlerFront/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
